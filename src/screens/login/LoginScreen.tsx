52edef124c3727a9de6adee8b1de62c30eace1a1
hIt Vir <hienhao021198@gmail.com>
2023-06-29 10:49:12 +0700

    test husky, lint-staged

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { scale } from 'themes/scales';

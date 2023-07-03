fbf61921d0e908a43605d758f64d1cf072b102a0
khanhhq173191 <khanh.ho@sotatek.com>
2023-07-03 10:17:40 +0700

    feat: form input

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,24 +1,54 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+import { loginDataForm } from './src/const';
+import loginSchema from './src/schema';
+
+import { FormInput } from 'components';
 import { scale } from 'themes/scales';
 import Sizes from 'themes/sizes';
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const {
+        control,
+        formState: { errors },
+        handleSubmit,
+        register,
+    } = useForm({
+        mode: 'all',
+        defaultValues: { email: '', password: '' },
+        resolver: yupResolver(loginSchema),
+    });
+
+    const onSubmit = async (data) => {
+        console.log(data);
+    };
 
     return (
         <View style={styles.container}>
             <View style={styles.content}>
-                <View>
-                    <TextInput placeholder="Email" value={email} onChangeText={setEmail} keyboardType="email-address" />
-                </View>
-                <View>
-                    <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+                <View style={styles.formRegister}>
+                    <FormInput
+                        control={control}
+                        name={loginDataForm[0].name}
+                        error={errors[`${loginDataForm[0].name}`]}
+                        placeholder={loginDataForm[0].name}
+                        label={loginDataForm[0].label}
+                        register={register}
+                    />
+                    <FormInput
+                        control={control}
+                        name={loginDataForm[1].name}
+                        error={errors[`${loginDataForm[1].name}`]}
+                        placeholder={loginDataForm[1].name}
+                        label={loginDataForm[1].label}
+                        register={register}
+                    />
                 </View>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleSubmit(onSubmit)}>
                     <Text>Login</Text>
                 </TouchableOpacity>
             </View>
@@ -36,5 +66,9 @@ const styles = StyleSheet.create({
     },
     content: {
         marginHorizontal: scale(15),
+    },
+    formRegister: {
+        height: scale(200),
+        justifyContent: 'space-around',
     },
 });
